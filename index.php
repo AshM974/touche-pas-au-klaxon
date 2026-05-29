d654102f27b136a36a56e91405e1b717338bc2f4
Alexia <alexia.velio@gmail.com>
2026-05-29 18:10:54 +0200

    Page d'acceuil  php ajout condition login

--- index.php
+++ index.php
@@ -3,20 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Touche Pas Au Klaxon</title>
+    <title>Touche pas au klaxon</title>
 </head>
 <body class=" container mt-3">
 <div class="navbar border border-dark rounded-4 px-3 py-2">
     <h1>Touche Pas Au Klaxon</h1>
     <nav>
-       <button class="btn btn-dark">
-            Connexion
-        </button>
+        <?php 
+        $login = false;
+        ?>
+
+        <?php if ($login) : ?>
+            <div class="d-flex justify-content-end align-items-center gap-3">
+            <button class="btn btn-dark">
+                Créer  un trajet
+            </button>
+            <p class="mb-0">Bonjour John Doe</p>
+            <button class="btn btn-dark">
+                Deconnexion
+            </button> 
+            </div>
+            
+       
+        <?php else : ?>
+            <button class="btn btn-dark">
+                Connexion
+            </button>
+        
+        <?php endif; ?>
+
+        
+        
     </nav>
 </div>
-    <p class="text-center">Pour obtenir plus d'informations sur un trajet, veuillez  vous  connecter</p>
 
-<div  >
+            <?php if ($login): ?>
+        <h2 class="fs-2">Trajets proposés</h2>
+            <?php else : ?>
+        <p class="fs-3 text-center">Pour obtenir plus d'informations sur un trajet, veuillez  vous  connecter</p>
+        <?php endif; ?>
+
+<div>
     <table class="border border-dark rounded-4 p-3 table">
 
     <thead>
@@ -28,6 +55,9 @@
             <th>Date</th>
             <th>Heure</th>
             <th>Places</th>
+            <?php if ($login) : ?>
+            <th >Actions</th>
+            <?php endif; ?>
         </tr>
     </thead>
     <tbody>
@@ -39,6 +69,9 @@
             <td>29/05/2026</td>
             <td>12:00</td>
             <td>3</td>
+            <?php if ($login) : ?>
+            <td ></td>
+            <?php endif; ?>
         </tr>
         <tr>
             <td>Lille</td>
@@ -48,6 +81,9 @@
             <td>30/05/2026</td>
             <td>14:00</td>
             <td>2</td>
+            <?php if ($login) : ?>
+            <td ></td>
+            <?php endif; ?>
         </tr>
         <tr>
             <td>Saint Denis</td>
@@ -57,6 +93,9 @@
             <td>02/06/2026</td>
             <td>17:00</td>
             <td>1</td>
+            <?php if ($login) : ?>
+            <td ></td>
+            <?php endif; ?>
         </tr>
     </tbody>
     <!-- <tfoot>
