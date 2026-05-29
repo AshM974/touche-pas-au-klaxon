285caa3da094c2caecc9f6cbefb41bbe845f4311
Alexia <alexia.velio@gmail.com>
2026-05-29 16:57:38 +0200

    Refonte de l'arborescence et  Reinitialisation du projet + Page acceuil fait dans le "dur"

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Touche Pas Au Klaxon</title>
+</head>
+<body class=" container mt-3">
+<div class="navbar border border-dark rounded-4 px-3 py-2">
+    <h1>Touche Pas Au Klaxon</h1>
+    <nav>
+       <button class="btn btn-dark">
+            Connexion
+        </button>
+    </nav>
+</div>
+    <p class="text-center">Pour obtenir plus d'informations sur un trajet, veuillez  vous  connecter</p>
+
+<div  >
+    <table class="border border-dark rounded-4 p-3 table">
+
+    <thead>
+        <tr class="table-dark">
+            <th>Départ</th>
+            <th>Date</th>
+            <th>Heure</th>
+            <th>Destinateur</th>
+            <th>Date</th>
+            <th>Heure</th>
+            <th>Places</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Paris</td>
+            <td>29/05/2026 </td>
+            <td>08:00</td>
+            <td>Lyon</td>
+            <td>29/05/2026</td>
+            <td>12:00</td>
+            <td>3</td>
+        </tr>
+        <tr>
+            <td>Lille</td>
+            <td>30/05/2026</td>
+            <td>09:00</td>
+            <td>Bordeaux</td>
+            <td>30/05/2026</td>
+            <td>14:00</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>Saint Denis</td>
+            <td>02/06/2026</td>
+            <td>15:00</td>
+            <td>Sainte-Marie</td>
+            <td>02/06/2026</td>
+            <td>17:00</td>
+            <td>1</td>
+        </tr>
+    </tbody>
+    <!-- <tfoot>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>    
+    </tfoot> -->
+    </table>
+    
+</div>
+
+<?php
+
+echo "";
+
+?>
+<footer>
+    <p class="text-center">© 2024 - CENEF - MVC PHP</p>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
