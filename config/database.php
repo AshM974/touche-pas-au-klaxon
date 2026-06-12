3b0efc139836828459a477c1006bfcf4de6bf3fd
Alexia <alexia.velio@gmail.com>
2026-06-12 17:49:26 +0200

    Installation du git ignore / Securisation de la BDD

--- config/database.php
+++ config/database.php
@@ -1,11 +1,18 @@
 <?php
 
-$host = "localhost";
-$dbname = "touche_pas_au_klaxon";
-$username = "test";
-$password = "1234";
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+
+$host = trim($_ENV['DB_HOST']);
+$dbname = trim($_ENV['DB_NAME']);
+$username = trim($_ENV['DB_USER']);
+$password = trim($_ENV['DB_PASSWORD']);
 
 try {
+
     $pdo = new PDO(
         "mysql:host=$host;dbname=$dbname;charset=utf8",
         $username,
@@ -14,9 +21,8 @@ try {
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
 } catch (PDOException $e) {
-    echo "Erreur : " . $e->getMessage();
-}
 
-?>
+    echo "Erreur : " . $e->getMessage();
+
+}
